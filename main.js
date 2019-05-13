9112d5b6df83631fd6bb2a98031bc98f2db573d9
jason parra gomez <jasonparra96@gmail.com>
2019-05-13 00:26:18 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,79 @@
+
+const SHA256 = require('crypto-js/sha256')
+
+class Block{
+    constructor(index, timestamp,data,previousHash=""){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce=0
+        
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0,difficulty)!== Array(difficulty+1).join("0")){
+            this.nonce++;
+            this.hash=this.calculateHash()
+        }
+        console.log("block mined: " + this.hash)
+    }
+
+}
+
+
+class Blockchain{
+    
+    constructor(){
+        this.chain=[this.createGenesisBlock()]
+        this.difficulty = 5;
+    }
+
+    createGenesisBlock(){
+        return new Block(0,"5/12/2019", "Genesis block","0")
+    }
+
+    getLeatestBlock(){
+        return this.chain[this.chain.length - 1]
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLeatestBlock().hash;
+        newBlock.mineBlock(this.difficulty)
+        this.chain.push(newBlock)
+    }
+
+    isChainValid(){
+        
+        for(let i=1; i< this.chain.length; i++)
+        {
+            const currentBlock = this.chain[i];
+            const previousblock = this.chain[i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash())
+            {
+                return false
+            }
+
+            if(currentBlock.previousHash !== previousblock.hash){
+                return false
+            }
+
+        }   
+
+        return true
+        
+    }
+}
+
+let platanoCoin = new Blockchain();
+console.log("Mining Block 1 .....")
+platanoCoin.addBlock(new Block(1,"5/12/2017"),{amount:4})
+console.log("Mining Block 2 .....")
+platanoCoin.addBlock(new Block(2,"5/12/2017"),{amount:12})
+
